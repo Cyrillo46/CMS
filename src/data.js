8007f1670b4aea9671784429d6c36251ae281acf
Cyrillo46 <yahooisbetter46@gmail.com>
2024-08-28 13:32:43 -0400

    added data file

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    id: 1,
+    img: Stacy,
+    url: "https://stacysdash.netlify.app/",
+    github: "https://github.com/Cyrillo46/Stacy-s-Dash",
+    title: "Stacy's Dash",
+    text: "A dynamic home dashboard that displays the current date, time, and stunning background photos. It pulls real-time data from Unsplash, Open Weather App, and Coin Gecko APIs. Built with vanilla JavaScript, this project highlights essential frontend skills like DOM manipulation and API integration.",
+  },
+  {
+    id: 2,
+    img: Tour,
+    url: "https://five-tours.netlify.app/",
+    github: "https://github.com/Cyrillo46/tours",
+    title: "Five Tours",
+    text: "A simple tour listing website that fetches tour data from a specified API and displays it to the user. The app includes features to handle loading states, refreshing tours, and removing individual tours from the list.",
+  },
+  {
+    id: 3,
+    img: Review,
+    url: "https://four-reviews.netlify.app/",
+    github: "https://github.com/Cyrillo46/four-reviews",
+    title: "Four Reviews",
+    text: "A dynamic React application to showcase user reviews, employing React Hooks for efficient state management and ensuring a seamless user experience.",
+  },
+];
