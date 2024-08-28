f6e3a88e0c41acb9eb5ffd4db1099169e9296dec
Cyrillo46 <yahooisbetter46@gmail.com>
2024-08-28 13:13:33 -0400

    structure for hero component complete

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,3 +1,19 @@
+import img from "./assets/cms-img.svg";
+
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="py-10 px-5 flex">
+      <div className="flex flex-col w-1/2 justify-center items-center px-12">
+        <h1 className="text-5xl">Contentful CMS</h1>
+        <p className="text-3xl tracking-wide capitalize">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+          reiciendis, ut ab, ullam quibusdam doloribus, debitis atque quos
+          laudantium eos aliquam nulla ducimus neque illo?
+        </p>
+      </div>
+      <div className="w-1/2">
+        <img src={img} alt="hero image" className="h-80 px-16" />
+      </div>
+    </section>
+  );
 };
